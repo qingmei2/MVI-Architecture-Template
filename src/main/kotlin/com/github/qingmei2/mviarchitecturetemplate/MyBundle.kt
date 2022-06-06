91e11456a11d631508e4939f9f49fec77f3c7ed0
GitHub Action <action@github.com>
2022-06-06 03:05:03 +0000

    Template cleanup

--- src/main/kotlin/com/github/qingmei2/mviarchitecturetemplate/MyBundle.kt
+++ src/main/kotlin/com/github/qingmei2/mviarchitecturetemplate/MyBundle.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template
+package com.github.qingmei2.mviarchitecturetemplate
 
 import com.intellij.DynamicBundle
 import org.jetbrains.annotations.NonNls
---
+++ src/main/kotlin/com/github/qingmei2/mviarchitecturetemplate/services/MyApplicationService.kt
@@ -0,0 +1,10 @@
+package com.github.qingmei2.mviarchitecturetemplate.services
+
+import com.github.qingmei2.mviarchitecturetemplate.MyBundle
+
+class MyApplicationService {
+
+    init {
+        println(MyBundle.message("applicationService"))
+    }
+}
